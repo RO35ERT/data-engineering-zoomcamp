2d7f5a69807e6c8b58927fd7364c111607b4285c
Phiri Robert <98536689+RO35ERT@users.noreply.github.com>
2026-05-16 15:28:12 +0000

    init pipeline

--- docker-workshop/pipeline.py
+++ docker-workshop/pipeline.py
@@ -1,3 +1,10 @@
 import sys
+import pandas as pd
 
-print("Hello, World!")
+print("Welcome to the pipeline")
+
+month = int(sys.argv[1])
+
+row = pd.DataFrame({"month": [month], "status": ["success"]})
+
+row.to_parquet("data.parquet")
